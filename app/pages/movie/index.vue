745003316bfb8e5f9a5b43b654c4730925257b8c
marco94gug <marco.guglielmino94@gmail.com>
2024-07-21 12:22:30 +0200

    init commit

---
+++ app/pages/movie/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Page movie</div>
+</template>
+
+<script setup lang="ts">
+const { data } = useNuxtData("movies");
+
+const asd = await useFishFetch("discover/movie", {
+  params: {
+    include_adult: false,
+    include_video: true,
+    language: "en-US",
+    page: 1,
+    sort_by: "popularity.desc",
+  },
+});
+console.log(asd);
+</script>
